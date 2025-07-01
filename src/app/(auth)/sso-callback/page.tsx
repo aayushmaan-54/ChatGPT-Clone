2e3117d4540996f4e92c2b426bd8482b38ca3424
Aayushmaan Soni <aayushmaan.soni54@gmail.com>
2025-07-01 13:08:21 +0530

    Update page.tsx

--- src/app/(auth)/sso-callback/page.tsx
+++ src/app/(auth)/sso-callback/page.tsx
@@ -6,7 +6,7 @@ import { Loader } from "lucide-react";
 
 export default function SSOCallback() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center w-screen min-h-screen">
       <div className="text-center">
         <Loader className="w-16 h-16 mx-auto mb-4 animate-spin" />
         <p className="text-lg font-bold">Completing sign up...</p>
